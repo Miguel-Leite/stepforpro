aa526376a95aca567808d8e8a8e7a9b4a3318959
Miguel Leite <miguelleite200leite@gmail.com>
2022-07-20 13:55:28 +0100

    🚀 class router done

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,24 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace app\core;
 
 class Router {
 
+    private $folderApp;
     private array $handlers;
+    private $notFoundHandler;
     private const METHOD_POST = 'POST';
     private const METHOD_GET = 'GET';
 
-    public function get(string $path, string $handler): void 
+    public function __construct(string $folderApp)
+    {
+        $this->folderApp = $folderApp;
+    }
+
+    public function get(string $path, callable | string $handler): void 
     {
         $this->addHandler(self::METHOD_GET,$path,$handler);
     }
 
-    public function post(string $path, string $handler): void 
+    public function post(string $path, callable | string $handler): void 
     {
         $this->addHandler(self::METHOD_POST,$path,$handler);
     }
 
-    private function addHandler(string $method, string $path, string | array $handler): void
+    public function addNotFoundHandler($handler): void
+    {
+        $this->notFoundHandler = $handler;
+    }
+
+    private function addHandler(string $method, string $path, callable | string  $handler): void
     {
         $this->handlers[$method . $path] = [
             'path' => $path,
@@ -30,7 +44,35 @@ class Router {
     public function dispatch()
     {
         $requestUri = parse_url($_SERVER['REQUEST_URI']);
-        $requestPath = $requestUri['path'];
-        var_dump($requestPath);
+        $requestPath = str_replace("/$this->folderApp",'',$requestUri['path']);
+        $method = $_SERVER["REQUEST_METHOD"];
+
+        $callback = null;
+        foreach ($this->handlers as $handler) {
+            if ($handler['path'] === $requestPath && $handler['method'] === $method) {
+                $callback = $handler['handler'];
+            }
+        }
+        if (!$callback) {
+            header('HTTP/1.0 404 Not Found');
+            if (!empty($this->notFoundHandler)) {
+                $callback = $this->notFoundHandler;
+            }
+        }
+
+        if (is_string($callback)) {
+            $parts = explode('::',$callback);
+            if (is_array($parts)) {
+                $className = "app\\services\\".array_shift($parts);
+                $handler = new $className();
+
+                $method = array_shift($parts);
+                $callback = [$handler, $method];
+            }
+        }
+        call_user_func_array($callback,[
+            array_merge($_GET, $_POST)
+        ]
+        );
     }
 }
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+use app\core\Router;
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -6,7 +9,17 @@ session_start();
 
 require_once 'bootstrap.php';
 
-use app\services\PagesServices;
 
-$service = new PagesServices();
-$service->index();
+$router = new Router('StepForPro');
+$router -> get('/','PagesServices::index');
+$router -> get('/about',function() {
+    echo "About Page";
+});
+
+$router->addNotFoundHandler(function () {
+    require_once __DIR__. DIRECTORY_SEPARATOR
+    . "app" . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR .
+    "error" . DIRECTORY_SEPARATOR . "404.php";
+});
+
+$router->dispatch();
