9259a93e1c1ab047a7436ca0cc88311912ad759b
Miguel Leite <miguelleite200leite@gmail.com>
2022-07-20 17:41:34 +0100

    🚀 import axios and remove lib not use in project

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,23 @@ AOS.init({duration: 2000})
 window.addEventListener('scroll', () => {
     document.querySelector('.btn-scroll').classList.toggle("btn-scroll-show", window.scrollY > 100)
 })
+
+if (document.querySelector('.form-login'))
+{
+    document.querySelector('.form-login').addEventListener('submit',async (e) => {
+        e.preventDefault()
+        const fd = new FormData(e.currentTarget);
+
+        const { data } = await axios.post(document.forms[0].action,fd);
+        if (data.success) {
+            // iziToast.success({
+            //     title: 'Sucesso!',
+            //     message: data.message,
+            //     position: 'topRight'
+            //   });
+            console.log(data)
+        } else {
+            console.log(data)
+        }
+    });
+}
