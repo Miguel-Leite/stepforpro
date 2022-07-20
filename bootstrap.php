61f970f20fd26589c73bed6373fe1baf18368ec5
Miguel Leite <miguelleite200leite@gmail.com>
2022-07-20 14:07:35 +0100

    🚀 load utils

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,18 @@
 <?php
 
 require_once 'app/autoload.php';
+
+/**
+ * Array responsavel por registrar todas funções que se 
+ * encontram na pasta utils.
+ */
+
+$loadUtils = [
+    "utils"
+];
+/**
+ * Carregando as funções registrada
+ */
+foreach ($loadUtils as $util) {
+    require_once __DIR__ . '/app/utils/'.$util.'.php';
+}
